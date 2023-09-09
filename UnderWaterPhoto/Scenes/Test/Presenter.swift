4ae13f8ef8c25939a3821f1a69da69440d232d3f
Maksim <mayden1127@yandex.ru>
2023-09-10 01:42:42 +0300

    Add WhiteBalance changing

--- UnderWaterPhoto/Scenes/Test/Presenter.swift
+++ UnderWaterPhoto/Scenes/Test/Presenter.swift
@@ -5,8 +5,11 @@
 //  Created by Максим Косников on 03.09.2023.
 //
 
+import CoreImage
+import UIKit
+
 protocol PresenterProtocol: AnyObject {
-    
+    func changeImage(image: UIImage)
 }
 
 class Presenter {
@@ -21,8 +24,31 @@ class Presenter {
     init(sceneBuildManager: Buildable) {
         self.sceneBuildManager = sceneBuildManager
     }
+    
+    func adjustWhiteBalance(image: UIImage) -> UIImage {
+        let ciImage = CIImage(image: image)
+        
+        let filter = CIFilter(name: "CITemperatureAndTint")
+        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+        filter?.setValue(CIVector(x: 10000, y: 0), forKey: "inputTargetNeutral")
+        
+        if let outputImage = filter?.outputImage {
+            let context = CIContext(options: nil)
+            
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+        return UIImage(named: "emptyImage")!
+    }
 }
 
 extension Presenter: PresenterProtocol {
+    func changeImage(image: UIImage) {
+        var newImage = UIImage()
+        newImage = adjustWhiteBalance(image: image)
+        
+        viewController?.uploadImage(image: newImage)
+    }
     
 }
--- UnderWaterPhoto/Scenes/Test/ViewController.swift
+++ UnderWaterPhoto/Scenes/Test/ViewController.swift
@@ -6,20 +6,156 @@
 //
 
 import UIKit
+import PhotosUI
 
-protocol ViewControllerProtocol: UIViewController {}
 
-class ViewController: UIViewController {
+// MARK: - ViewControllerProtocol
+
+protocol ViewControllerProtocol: UIViewController {
+    func uploadImage(image: UIImage)
+}
+
+// MARK: - ViewController
+
+final class ViewController: UIViewController {
+    
     var presenter: PresenterProtocol?
     
+    // MARK: PrivateProperties
+    
+    private var imagePicker = UIImagePickerController()
+    
+    private let mainImage: UIImageView = {
+        let image = UIImage(named: "emptyImage")
+        let imageView = UIImageView()
+        imageView.image = image
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let commonStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.alignment = .center
+        return stack
+    }()
+    
+    private lazy var uploadPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Загрузить фото", for: .normal)
+        button.layer.cornerRadius = 8
+        button.backgroundColor = .systemBlue
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(uploadButtonAction), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var changePhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Отредактировать", for: .normal)
+        button.layer.cornerRadius = 8
+        button.backgroundColor = .systemBlue
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(changeButtonAction), for: .touchUpInside)
+        return button
+    }()
+    
+    private let buttonsStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 16
+        stack.alignment = .center
+        stack.distribution = .fillEqually
+        return stack
+    }()
+    
+    // MARK: LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .blue
+        setupViewController()
     }
-
-
+    
+    // MARK: Action
+    
+    @objc
+    func changeButtonAction() {
+        presenter?.changeImage(image: self.mainImage.image!)
+    }
+    
 }
 
-extension ViewController: ViewControllerProtocol {}
+// MARK: - ViewControllerProtocol Imp
+
+extension ViewController: ViewControllerProtocol {
+    func uploadImage(image: UIImage) {
+        DispatchQueue.main.async {
+            self.mainImage.image = image
+        }
+    }
+}
+
+// MARK: - PrivateMethods
+
+extension ViewController {
+    func setupViewController() {
+        addSubviews()
+        setupConstraints()
+        setupNavigationController()
+        view.backgroundColor = .white
+    }
+    
+    func setupNavigationController() {
+        
+    }
+    
+    func addSubviews() {
+        view.addSubviews(commonStackView, buttonsStackView)
+        
+        commonStackView.addArrangedSubviews(mainImage)
+        
+        buttonsStackView.addArrangedSubviews(uploadPhotoButton,
+                                             changePhotoButton)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            // commonStackView with mainImage
+            commonStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: commonStackView.trailingAnchor, constant: 16),
+            commonStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            commonStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -200),
+            // buttonsStackView
+            buttonsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: buttonsStackView.trailingAnchor, constant: 16),
+            buttonsStackView.topAnchor.constraint(equalTo: mainImage.bottomAnchor, constant: 16)
+        ])
+    }
+}
+
+extension ViewController: PHPickerViewControllerDelegate {
+    @objc
+    func uploadButtonAction() {
+        var config = PHPickerConfiguration()
+        config.filter = PHPickerFilter.images
+        
+
+        let pickerViewController = PHPickerViewController(configuration: config)
+        
+        
+        pickerViewController.delegate = self
+        self.present(pickerViewController, animated: true, completion: nil)
+    }
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true, completion: nil)
+           for result in results {
+              result.itemProvider.loadObject(ofClass: UIImage.self, completionHandler: { (object, error) in
+                 if let image = object as? UIImage {
+                     DispatchQueue.main.async {
+                         self.mainImage.image = image
+                     }
+                 }
+              })
+           }
+    }
+}
