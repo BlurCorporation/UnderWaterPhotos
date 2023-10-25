4df72344b16da2529d9777d7a2bbd79e9d6a66fa
Maksim <mayden1127@yandex.ru>
2023-10-25 10:10:07 +0300

    Add C++ imageProcessing

--- UnderWaterPhoto/Scenes/Test/Presenter.swift
+++ UnderWaterPhoto/Scenes/Test/Presenter.swift
@@ -27,22 +27,19 @@ class Presenter {
         self.sceneBuildManager = sceneBuildManager
         self.imageProcessingManager = imageProcessingManager
     }
-    
-    
 }
 
 extension Presenter: PresenterProtocol {
     func changeImage(image: UIImage, value: Float) {
-        var newImage = UIImage()
-        newImage = imageProcessingManager.adjustExposure(image: image, exposure: 0.2)
-        newImage = imageProcessingManager.adjustContrast(image: newImage, contrast: 1.1)
-        newImage = imageProcessingManager.adjustBrightnessForShadows(image: newImage, brightness: 0.2)
-//        newImage = imageProcessingManager.adjustWhiteBalance(image: image, value)
-//        newImage = imageProcessingManager.sharpenImage(image: image, sharpness: CGFloat(value))
-//        newImage = imageProcessingManager.adjustBrightnessForHighlights(image: image, brightness: CGFloat(value))
-//        newImage = imageProcessingManager.adjustHue(image: image, hue: CGFloat(value))
-//        newImage = imageProcessingManager.adjustSaturation(image: image, saturation: CGFloat(value))
-//        newImage = imageProcessingManager.adjustColorfulness(image: image, factor: CGFloat(value))
-        viewController?.uploadImage(image: newImage)
+        
+        Task {
+            let newImage: UIImage = try await process(image: image)
+            self.viewController?.uploadImage(image: newImage)
+        }
+    }
+    
+    func process(image: UIImage) async throws -> UIImage{
+        let newImage = try CVWrapper.process(withImages: image)
+        return newImage
     }
 }
