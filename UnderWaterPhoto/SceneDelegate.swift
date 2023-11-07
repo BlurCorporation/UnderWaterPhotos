db01cf22f00024cfd8edce635fd6d2e1f098649d
Maksim <mayden1127@yandex.ru>
2023-11-07 17:31:29 +0300

    Change opencv dependencie

--- UnderWaterPhoto/SceneDelegate.swift
+++ UnderWaterPhoto/SceneDelegate.swift
@@ -25,7 +25,7 @@ private extension SceneDelegate {
     func setupRootViewController(windowScene: UIWindowScene) {
         let window = UIWindow(windowScene: windowScene)
         let sceneBuildManager: Buildable = SceneBuildManager()
-        let viewController = sceneBuildManager.buildMainView()
+        let viewController = sceneBuildManager.buildViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
