f91c4897e6e898d68103a5634ffa5dca84aac899
Anton Mironenko <103453526+oknenorimAnton@users.noreply.github.com>
2024-01-21 15:13:54 +0300

    add providers and authService

---
+++ UnderWaterPhoto/Core/AuthRequestsModels/RegisterUserRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterUserRequest.swift
+//  ARHitBalls
+//
+//  Created by Максим Косников on 22.07.2023.
+//
+
+import Foundation
+
+struct RegisterUserRequest {
+    let email: String
+    let password: String
+}
